44a144c0cd57135c2811ffa8c428352e64166988
Hannah Tabunan <hannah.tabunan@gmail.com>
2020-12-24 14:51:29 +0800

    Updated webpack script

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +11,24 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+
+// copy images folder into laravel public folder
+mix.copyDirectory('resources/assets/metronic/src/assets/media', 'public/assets/media');
+mix.copyDirectory('resources/assets/media/logos', 'public/assets/media/logos');
+
+/**
+ * plugins specific issue workaround for webpack
+ * @see https://github.com/morrisjs/morris.js/issues/697
+ * @see https://stackoverflow.com/questions/33998262/jquery-ui-and-webpack-how-to-manage-it-into-module
+ */
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            'morris.js': 'morris.js/morris.js',
+            'jquery-ui': 'jquery-ui',
+        },
+    },
+});
