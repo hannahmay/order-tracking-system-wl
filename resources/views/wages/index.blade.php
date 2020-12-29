f6e34b13fc411f542852a429134fb0a2772d9cbc
Hannah May Tabunan <hannah_may@lapapa88.com>
2020-12-29 14:13:54 +0800

    Updated wages.index view

--- resources/views/wages/index.blade.php
+++ resources/views/wages/index.blade.php
@@ -22,48 +22,14 @@
                 </div>
             </div>
         </div>
-        <div class="kt-portlet__body">
-            <!--begin: Search Form -->
-            <form class="kt-form kt-form--label-right">
-                <div class="kt-portlet__body">
-                    <div class="form-group row">
-                        <div class="col-lg-4">
-                            <label class="">Search:</label>
-                            <div class="kt-input-icon kt-input-icon--right">
-                                <input type="text" class="form-control" placeholder="Search..." id="generalSearch">
-                                <span class="kt-input-icon__icon kt-input-icon__icon--right">
-                                    <span><i class="la la-search"></i></span>
-                                </span>
-                            </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <label class="">Status:</label>
-                            <select class="form-control kt-select2" id="search_select_status">
-                                <option></option>
-                                <!-- TODO: Dynamically define the status options -->
-                                <option value="E">Enabled</option>
-                                <option value="D">Disabled</option>
-                            </select>
-                        </div>
-                        <div class="col-lg-4">
-                            <label class="">Registered Date:</label>
-                            <div class='input-group pull-right' id="search_daterangepicker">
-                                <input type='text' class="form-control" readonly placeholder="Select date range" />
-                                <div class="input-group-append">
-                                    <span class="input-group-text"><i class="la la-calendar-check-o"></i></span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
-            <!--end: Search Form -->
-        </div>
         <div class="kt-portlet__body kt-portlet__body--fit">
-
-            <!--begin: Datatable -->
-            <div class="kt-datatable" id="ajax_data"></div>
-            <!--end: Datatable -->
+            <div class="row row-no-padding row-col-separator-xl">
+                @foreach ($wages as $wage)
+                    <div class="col-md-12 col-lg-12 col-xl-4">
+                        @include('layout.widgets.stats', ['wage'=>$wage])
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
     <!--end::Portlet-->
