e82b8cf832ee8db52a057badb87a3229466ad4a6
Hannah May Tabunan <hannah_may@lapapa88.com>
2021-02-10 17:40:42 +0800

    Added custom blade directives

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::directive('activeIfInRouteGroup', function ($expression) {
+            return "Route::currentRouteNamed($expression) ? 'kt-menu__item--open kt-menu__item--here' : 'hannah'";
+        });
+
+        Blade::directive('getRouteName', function () {
+            return "Request::segment(1) ? Request::segment(1) : Route::current()->getName()";
+        });
+
+        Blade::directive('getTime', function ($expression) {
+            return "($expression)->format('h:i')";
+        });
     }
 }
